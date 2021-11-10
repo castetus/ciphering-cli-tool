8858c64028c60babd7f4c9fc6d1956ee13006abf
castetus <kboikov@mail.ru>
2021-11-10 11:38:24 +0300

    fix: disable eslint no-process-exit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,5 +13,6 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
+    'no-process-exit': 'off'
   }
 }
--- CustomError.js
+++ CustomError.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-process-exit */
 export default class CustomError {
 
   duplicate(key) {
